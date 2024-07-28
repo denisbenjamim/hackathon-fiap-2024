38e3e0a3f3579b7b3f7658135a82d6fd0b3fa22a
Denis Benjamim de Souza Alves <denis.benjamim@gmail.com>
2024-07-27 23:34:11 -0300

    Ajustado Id para JPA Repository de Clientes

--- src/main/java/br/com/fiap/hackathon/spring/repository/ClientesRepositoryJPA.java
+++ src/main/java/br/com/fiap/hackathon/spring/repository/ClientesRepositoryJPA.java
@@ -1,6 +1,5 @@
 package br.com.fiap.hackathon.spring.repository;
 
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +8,7 @@ import br.com.fiap.hackathon.spring.entity.ClienteEntity;
 
 
 @Repository
-public interface ClientesRepositoryJPA extends JpaRepository<ClienteEntity, UUID> {
+public interface ClientesRepositoryJPA extends JpaRepository<ClienteEntity, String> {
     
     public ClienteEntity findByCpf(String cpf);
     public boolean existsByCpf(String cpf);
