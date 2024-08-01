38ff7e94dfeb34ea2c18a902f88516f525b39744
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2024-08-01 00:46:17 -0300

    Criada as classes AutenticacoesController e AuthenticationController

--- src/main/java/br/com/fiap/hackathon/core/controller/AuthenticationController.java
+++ src/main/java/br/com/fiap/hackathon/core/controller/AuthenticationController.java
@@ -1,24 +1,24 @@
-//package br.com.fiap.hackathon.core.controller;
-//
-//import br.com.fiap.hackathon.core.exception.BusinessException;
-//import br.com.fiap.hackathon.core.input.AuthenticationInput;
-//import br.com.fiap.hackathon.core.output.LoginResponseOutput;
-//import br.com.fiap.hackathon.core.usecases.autenticacao.AuthenticationUseCase;
-//import br.com.fiap.hackathon.core.vo.autenticacao.AuthenticationVo;
-//
-//public abstract class AuthenticationController {
-//
-//    final AuthenticationUseCase authenticationUseCase;
-//
-//    public AuthenticationController(AuthenticationUseCase authenticationUseCase) {
-//        this.authenticationUseCase = authenticationUseCase;
-//    }
-//
-//    protected LoginResponseOutput login(AuthenticationInput input) throws BusinessException {
-//        return authenticationUseCase.login(input);
-//    }
-//
-//    protected void register(AuthenticationVo input) throws BusinessException {
-//        authenticationUseCase.register(input);
-//    }
-//}
+package br.com.fiap.hackathon.core.controller;
+
+import br.com.fiap.hackathon.core.exception.BusinessException;
+import br.com.fiap.hackathon.core.input.AuthenticationInput;
+import br.com.fiap.hackathon.core.output.LoginResponseOutput;
+import br.com.fiap.hackathon.core.usecases.autenticacao.AuthenticationUseCase;
+import br.com.fiap.hackathon.core.vo.autenticacao.AuthenticationVo;
+
+public abstract class AuthenticationController {
+
+    final AuthenticationUseCase authenticationUseCase;
+
+    protected AuthenticationController(AuthenticationUseCase authenticationUseCase) {
+        this.authenticationUseCase = authenticationUseCase;
+    }
+
+    protected LoginResponseOutput login(AuthenticationInput input) throws BusinessException {
+        return authenticationUseCase.login(input);
+    }
+
+    protected void register(AuthenticationVo input) throws BusinessException {
+        authenticationUseCase.register(input);
+    }
+}
--- src/main/java/br/com/fiap/hackathon/spring/controller/AutenticacoesController.java
+++ src/main/java/br/com/fiap/hackathon/spring/controller/AutenticacoesController.java
@@ -1,44 +1,72 @@
-//package br.com.fiap.hackathon.spring.controller;
-//
-//import br.com.fiap.hackathon.core.controller.AuthenticationController;
-//import br.com.fiap.hackathon.core.gateway.AutenticacaoRepository;
-//import br.com.fiap.hackathon.core.input.AuthenticationInput;
-//import br.com.fiap.hackathon.core.services.TokenService;
-//import br.com.fiap.hackathon.core.usecases.autenticacao.AuthenticationUseCase;
-//import br.com.fiap.hackathon.core.vo.autenticacao.AuthenticationVo;
-//import br.com.fiap.hackathon.spring.swagger.custom.ApiResponseCliente_201_400_500;
-//import br.com.fiap.hackathon.spring.utils.SpringControllerUtils;
-//import jakarta.validation.Valid;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/autenticacao")
-//public class AutenticacoesController extends AuthenticationController {
-//
-//    public AutenticacoesController(AutenticacaoRepository autenticacaoRepository, TokenService tokenService, AuthenticationManager authenticationManager) {
-//        super(new AuthenticationUseCase(autenticacaoRepository, tokenService, authenticationManager));
-//    }
-//
-//    @PostMapping("/login")
-//    @ApiResponseCliente_201_400_500
-//    public ResponseEntity<?> login(@RequestBody @Valid AuthenticationInput request) {
-//        var usernamePassword = new UsernamePasswordAuthenticationToken(request.login(), request.password());
-//        var auth = this.authenticationManager.authenticate(usernamePassword);
-//        var token = this.tokenService.generateToken((AuthenticationVo) auth.getPrincipal());
-//
-//        return SpringControllerUtils.response(HttpStatus.OK, () -> login(request));
-//    }
-//
-//    @PostMapping("/register")
-//    @ApiResponseCliente_201_400_500
-//    public ResponseEntity<?> register(@RequestBody @Valid AuthenticationInput request) {
-//        return SpringControllerUtils.response(HttpStatus.CREATED, () -> {
-//            register(request);
-//            return null;
-//        });
-//    }
-//}
+package br.com.fiap.hackathon.spring.controller;
+
+import br.com.fiap.hackathon.core.exception.BusinessException;
+import br.com.fiap.hackathon.core.gateway.AutenticacaoRepository;
+import br.com.fiap.hackathon.core.input.AuthenticationInput;
+import br.com.fiap.hackathon.core.output.LoginResponseOutput;
+import br.com.fiap.hackathon.core.services.TokenService;
+import br.com.fiap.hackathon.core.vo.autenticacao.AuthenticationVo;
+import br.com.fiap.hackathon.spring.entity.AutenticacaoEntity;
+import br.com.fiap.hackathon.spring.swagger.custom.ApiResponseCliente_200_400_500;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/autenticacao")
+public class AutenticacoesController {
+
+    private final AuthenticationManager authenticationManager;
+    private final AutenticacaoRepository autenticacaoRepository;
+    private final TokenService tokenService;
+
+    public AutenticacoesController(AutenticacaoRepository autenticacaoRepository, TokenService tokenService, AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+        this.autenticacaoRepository = autenticacaoRepository;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/login")
+    @ApiResponseCliente_200_400_500
+    public ResponseEntity<LoginResponseOutput> login(@RequestBody @Valid AuthenticationInput data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+        var token = this.tokenService.generateToken((AutenticacaoEntity) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseOutput(token));
+    }
+
+    @PostMapping("/currentUser")
+    public ResponseEntity<?> getUserByToken(@RequestParam("token") String token) throws BusinessException {
+        final String loginUsuario = tokenService.validateToken(token);
+
+        if (!StringUtils.hasText(loginUsuario)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var user = autenticacaoRepository.buscarPorLogin(loginUsuario);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(Map.of("idUsuario", user.getCodigo()));
+    }
+
+    @PostMapping("/register")
+    @ApiResponseCliente_200_400_500
+    public ResponseEntity<Void> register(@RequestBody @Valid AuthenticationVo data) throws BusinessException {
+        if (this.autenticacaoRepository.buscarPorLogin(data.getLogin()) != null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var user = new AutenticacaoEntity(data.getLogin(), data.getPassword());
+        this.autenticacaoRepository.salvar(user);
+
+        return ResponseEntity.ok().build();
+    }
+}
