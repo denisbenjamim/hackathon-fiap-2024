95388468bd0d680555c93f50bf7fffa63c9f4b2d
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2024-08-02 00:27:31 -0300

    Criada a classe SwaggerConfig

---
+++ src/main/java/br/com/fiap/hackathon/spring/swagger/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package br.com.fiap.hackathon.spring.swagger.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Hackathon API", version = "1.0", description = "Documentation for Hackathon API"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig implements WebMvcConfigurer {
+}
